9f240c6a24217494caaeaa8dd867267b1ba4db91
Anushka <anushkatiwari0425@gmail.com>
2024-10-23 23:05:19 +0530

    responsive statistics

--- frontend/src/components/LandingPage/Statistics.js
+++ frontend/src/components/LandingPage/Statistics.js
@@ -1,40 +1,42 @@
-import React from 'react'
+import React from 'react';
 import CountUp from 'react-countup';
 
 const Statistics = () => {
   return (
-    <div> 
-      <div className="flex flex-row justify-between mt-[200px]">
-        <div className="ml-[120px]">
-          <h1 className="text-4xl font-bold text-[#F7A221]">
-            <CountUp end={51} duration={2.5} />+
-          </h1>
-          <h3 className="text-xl">Enterprises</h3>
-        </div>
-        <div className="ml-[120px]">
-          <h1 className="text-4xl font-bold text-[#F7A221]">
-            <CountUp end={20} separator="," duration={3} />
-            M
-          </h1>
-          <h3 className="text-xl">Lives Impacted</h3>
-        </div>
-        <div className="ml-[120px] ">
-          <h1 className="text-4xl font-bold text-[#F7A221]">
-            <CountUp end={8000} separator="," duration={3.5} />
-            {/* + */}
-          </h1>
-          <h3 className="text-xl">Jobs Created</h3>
-        </div>
-        <div className="ml-[120px] mr-[120px]">
-          <h1 className="text-4xl font-bold text-[#F7A221]">
-            <CountUp end={5.8} separator="," duration={3} />
-            M
-          </h1>
-          <h3 className="text-xl">Seed Funding (INR)</h3>
-        </div>
-      </div>  
-    </div>
-  )
-}
+    <div className="flex flex-col md:flex-row justify-between items-center gap-8 md:gap-16 mt-20 mx-5 md:mx-20 mt-[150px]">
+      {/* Enterprises */}
+      <div className="flex flex-col items-center ">
+        <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold text-[#F7A221]">
+          <CountUp end={51} duration={2.5} />+
+        </h1>
+        <h3 className="text-lg md:text-xl">Enterprises</h3>
+      </div>
 
-export default Statistics
+      {/* Lives Impacted */}
+      <div className="flex flex-col items-center ">
+        <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold text-[#F7A221]">
+          <CountUp end={20} separator="," duration={3} /> M
+        </h1>
+        <h3 className="text-lg md:text-xl">Lives Impacted</h3>
+      </div>
+
+      {/* Jobs Created */}
+      <div className="flex flex-col items-center ">
+        <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold text-[#F7A221]">
+          <CountUp end={8000} separator="," duration={3.5} />+
+        </h1> 
+        <h3 className="text-lg md:text-xl">Jobs Created</h3>
+      </div>
+
+      {/* Seed Funding */}
+      <div className="flex flex-col items-center ">
+        <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold text-[#F7A221]">
+          <CountUp end={5.8} separator="," duration={3} /> M
+        </h1>
+        <h3 className="text-lg md:text-xl">Seed Funding (INR)</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
