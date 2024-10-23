79be0da0b1ef8cc046b79934c9c78df62f69114e
Anushka <anushkatiwari0425@gmail.com>
2024-10-23 22:47:17 +0530

    added statistics

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,15 +1,17 @@
-import Hero from '../components/LandingPage/Hero';
-import Navbar from '../components/Navbar';
-import Contact from '../components/ContactUs';
+import Hero from "../components/LandingPage/Hero";
+import Navbar from "../components/Navbar";
+import Contact from "../components/ContactUs";
+import Statistics from "../components/LandingPage/Statistics";
 
-const LandingPage = () =>{
-    return(
-        <div className="LandingPage">
-            <Navbar />
-            <Hero />
-            <Contact />
-        </div>
-    );
-}
+const LandingPage = () => {
+  return (
+    <div className="LandingPage">
+      <Navbar />
+      <Hero />
+      <Statistics />
+      <Contact />
+    </div>
+  );
+};
 
 export default LandingPage;
