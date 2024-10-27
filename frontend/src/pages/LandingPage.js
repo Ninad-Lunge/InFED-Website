809e49bbfb9629cf5b06ff9d82b8a31208ed3d82
Anushka <anushkatiwari0425@gmail.com>
2024-10-27 11:07:38 +0530

    changes updated

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -3,6 +3,8 @@ import Navbar from "../components/Navbar";
 import Contact from "../components/ContactUs";
 import Statistics from "../components/LandingPage/Statistics";
 import Objectives from "../components/LandingPage/Objectives";
+import Programme from "../components/LandingPage/Programme";
+import Partners from "../components/LandingPage/Partners";
 
 const LandingPage = () => {
   return (
@@ -11,6 +13,8 @@ const LandingPage = () => {
       <Hero />
       <Statistics />
       <Objectives />
+      <Programme />
+      <Partners />
       <Contact />
     </div>
   );
